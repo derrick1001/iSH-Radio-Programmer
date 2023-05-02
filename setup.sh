8f269b57cab129807cd654ee7c0366faba87be6e
Derrick Ishman <33956235+derrick1001@users.noreply.github.com>
2023-05-02 15:38:34 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,6 @@ apk add openssh
 apk add busybox-extras
 mv /etc/profile.d/color_prompt.sh.disabled /etc/profile.d/color_prompt.sh
 source /etc/profile.d/color_prompt.sh
-mv ash_alias.sh /etc/profile.d/
+mv iSH-scripts/ash_alias.sh /etc/profile.d/
 source /etc/profile.d/ash_alias.sh
 
