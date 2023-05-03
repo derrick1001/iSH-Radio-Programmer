0126107d642aaab0f7bfbf14cd42bbbe47f97208
Derrick Ishman <33956235+derrick1001@users.noreply.github.com>
2023-05-03 08:34:13 -0500

    Update ash_alias.sh

--- ash_alias.sh
+++ ash_alias.sh
@@ -2,4 +2,4 @@ alias go='/bin/sh /root/iSH-scripts/menu.sh'
 alias howto='cat /root/iSH-scripts/README'
 alias rmkwh='ssh-keygen -f "/root/.ssh/known_hosts" -R $1'
 alias telon='ssh-keygen -f "/root/.ssh/known_hosts" -R 192.168.1.1 ; ssh-keygen -f "/root/.ssh/known_hosts" -R 192.168.1.2 ; /bin/sh /root/iSH-scripts/telneton.sh | ssh admin@192.168.1.1 > /dev/null'
-
+alias sync='cd iSH-scripts && git pull'
