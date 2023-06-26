5c30c0d20bcd7ffb5151fdd4fc204536d243d0ce
Derrick Ishman <derrick.ishman@gmail.com>
2023-06-26 16:46:33 -0500

    Changed directory structure to sd1-part1

--- 850C-software.sh
+++ 850C-software.sh
@@ -3,7 +3,7 @@ echo "admin"
 sleep 2
 echo "admin"
 sleep 2
-echo "platform software download channel server set server-ip $1 directory sd2-part1/850C-Builds/850C-12-1 username RSI-Admin password can0py_BAM"
+echo "platform software download channel server set server-ip $1 directory sd1-part1/850C-Builds/850C-12-1 username RSI-Admin password can0py_BAM"
 sleep 5
 echo "platform software download version protocol ftp"
 sleep 5
