215a25d16a596ea1806379b85b9a77acb331fb82
Derrick Ishman <derrick.ishman@gmail.com>
2023-06-26 15:22:40 -0500

    Changed file directory from disk1 to sd2-part1

--- 850C-import-18Ghz-LO-template.sh
+++ 850C-import-18Ghz-LO-template.sh
@@ -3,7 +3,7 @@ echo "admin"
 sleep 2
 echo "admin"
 sleep 2
-echo "platform configuration channel server set ip-address $1 directory disk1/850C-Builds/ filename 850C-12-1-18Ghz-LO-template.zip username RSI-Admin password can0py_BAM"
+echo "platform configuration channel server set ip-address $1 directory sd2-part1/850C-Builds/ filename 850C-12-1-18Ghz-LO-template.zip username RSI-Admin password can0py_BAM"
 sleep 3
 echo "platform configuration configuration-file import restore-point-1"
 sleep 3
