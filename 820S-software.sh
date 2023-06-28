44542a89b34efd6198fa32097b6de259855d51e6
Derrick Ishman <derrick.ishman@gmail.com>
2023-06-28 16:23:30 -0500

    Changed directory structure... AGAIN, /sd1 is the root directory

--- 820S-software.sh
+++ 820S-software.sh
@@ -3,7 +3,7 @@ echo "admin"
 sleep 2
 echo "admin"
 sleep 2
-echo "platform software download channel server set server-ip $1 directory sd1-part1/820S-Builds/820S-12-0/ username RSI-Admin password can0py_BAM"
+echo "platform software download channel server set server-ip $1 directory sd1/820S-Builds/820S-12-0/ username RSI-Admin password can0py_BAM"
 sleep 5
 echo "platform software download version"
 sleep 5
