635ca6ecca8861b4fa7622227771c3c4ad09a2cb
Derrick Ishman <derrick.ishman@gmail.com>
2023-07-07 12:13:19 -0500

    Added countdown for reboot process, fixed socket closed error after reboot

--- ssh-test.py
+++ ssh-test.py
@@ -7,6 +7,7 @@ from time import sleep
 ip = input('IP-address: ')
 user = input('User: ')
 password = input('Password: ')
+radio = input('HI or LO: ').upper()
 
 
 def connect_to_radio(ip, user, password):
@@ -16,8 +17,14 @@ def connect_to_radio(ip, user, password):
               'password': password,
               'fast_cli': False
               }
-    conn = ConnectHandler(**device)
-    return conn
+    return ConnectHandler(**device)
+
+
+def countdown(seconds):
+    while seconds:
+        print(f'{seconds} remaining', end='\r')
+        sleep(1)
+        seconds -= 1
 
 
 con = connect_to_radio(ip='192.168.1.1', user='admin', password='admin')
@@ -31,13 +38,13 @@ def refresh(cmd):
             sleep(2)
             break
         else:
-            print(output)
+            print(output, end='\r')
             sleep(10)
 
 
 def software_dl():
     dl_software = 'platform software download version protocol ftp'
-    set_software_param = 'platform software download channel server set server-ip 192.168.1.30 directory sd1/820S-Builds/820S-11-9/ username RSI-Admin password can0py_BAM'
+    set_software_param = 'platform software download channel server set server-ip 192.168.1.30 directory sd1/820S-Builds/820S-12-0/ username RSI-Admin password can0py_BAM'
     status = 'platform software download status show'
 
     con.send_command(set_software_param)
@@ -51,6 +58,7 @@ def software_dl():
     print('Software successfully uploaded.')
     sleep(2)
     print('Upgrading firmware now..')
+    sleep(2)
     install_func()
 
 
@@ -67,15 +75,15 @@ def install_func():
     print('Installation complete.')
     sleep(2)
     print('Rebooting...')
-    sleep(185)
-    import_func(input('HI or LO: ').upper())
-
+    countdown(240)
+    import_func(radio)
 
 
 def import_func(radio_type):
     set_template_params = f'platform configuration channel server set ip-address 192.168.1.30 directory sd1/820S-Builds/ filename 820S-12-0-{radio_type}-template.zip username RSI-Admin password can0py_BAM'
     import_template = 'platform configuration configuration-file import restore-point-1'
     restore_template = 'platform configuration configuration-file restore restore-point-1'
+    con = connect_to_radio(ip='192.168.1.1', user='admin', password='admin')
 
     con.send_command(set_template_params)
     con.send_command(import_template,
@@ -91,7 +99,8 @@ def import_func(radio_type):
             print('Something went wrong..')
             break
         else:
-            print(output)
+            print(output, end='\r')
 
 
-software_dl()
+import_func(radio)
+# software_dl()
