600eebe3a2f6e59bb94ba8c5106e63592718d5bd
Derrick Ishman <derrick.ishman@gmail.com>
2023-07-20 11:59:27 -0500

    testing for ish app

--- a.py
+++ a.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3.8
 
 from netmiko import ConnectHandler
-from time import sleep
+# from time import await sleep
+from asyncio import run, sleep
 from rich.progress import track
 import colorama
 
@@ -21,7 +22,7 @@ password = input(f'{c_BLUE}Password: ')
 radio = input(f'{c_BLUE}HI or LO: ').upper()
 
 
-def connect_to_radio(ip, user, password):
+async def connect_to_radio(ip, user, password):
     device = {'device_type': 'generic',
               'host': ip,
               'username': user,
@@ -32,13 +33,13 @@ def connect_to_radio(ip, user, password):
     return conn
 
 
-def countdown(second):
+async def countdown(second):
     for n in track(range(second), description=f'{c_GREEN}Rebooting...'):
-        sleep(1)
-countdown(10)
+        await sleep(1)
+run(countdown(10))
 
 
-# def refresh(cmd):
+# async def refresh(cmd):
 #     while True:
 #         output = con.send_command(cmd)
 #         print(output)
@@ -53,16 +54,16 @@ countdown(10)
 #         elif 'download in progress' in output:
 #             parse_string = output.split()
 #             print(f"{c_CYAN}{parse_string[8].capitalize()} {c_CYAN}{parse_string[9].capitalize()} {c_MAGENTA}{parse_string[10].capitalize()}", end='\r')
-#             sleep(10)
+#             await sleep(10)
 #         elif 'install started' or 'installation in progress' in output:
 #             parse_string = output.split()
 #             print(f"{c_CYAN}{parse_string[8].capitalize()} {c_CYAN}{parse_string[9].capitalize()} {c_MAGENTA}{parse_string[10].capitalize()}", end='\r')
-#             sleep(10)
+#             await sleep(10)
 #         else:
 #             continue
 # 
 # 
-# def software_dl():
+# async def software_dl():
 #     dl_software = 'platform software download version protocol ftp'
 #     set_software_param = 'platform software download channel server set server-ip 192.168.1.30 directory sd1/820S-Builds/820S-11-9/ username RSI-Admin password can0py_BAM'
 #     status = 'platform software download status show'
@@ -75,16 +76,16 @@ countdown(10)
 #                      delay_factor=2
 #                      )
 #     print(f'{c_GREEN}Downloading software..')
-#     sleep(2)
+#     await sleep(2)
 #     refresh(status)
-#     sleep(3)
+#     await sleep(3)
 #     print(f'{c_GREEN}Software successfully uploaded.')
-#     sleep(2)
+#     await sleep(2)
 #     print(f'{c_GREEN}Upgrading firmware..')
 #     install_func()
 # 
 # 
-# def install_func():
+# async def install_func():
 #     install_software = 'platform software install version'
 #     status = 'platform software install status show'
 #     con.send_command(install_software,
@@ -94,14 +95,14 @@ countdown(10)
 #                      delay_factor=2
 #                      )
 #     refresh(status)
-#     sleep(3)
+#     await sleep(3)
 #     print(f'{c_GREEN}Installation complete.')
-#     sleep(2)
+#     await sleep(2)
 #     # countdown(250)
 #     # import_func(radio)
 # 
 # 
-# def import_func(radio_type):
+# async def import_func(radio_type):
 #     set_template_params = f'platform configuration channel server set ip-address 192.168.1.30 directory sd1/820S-Builds/ filename 820S-12-0-{radio_type}-template.zip username RSI-Admin password can0py_BAM'
 #     import_template = 'platform configuration configuration-file import restore-point-1'
 #     restore_template = 'platform configuration configuration-file restore restore-point-1'
@@ -113,7 +114,7 @@ countdown(10)
 #     con.send_command('yes\n',
 #                      expect_string='root>',)
 #     print(f'{c_GREEN}Importing template..')
-#     sleep(2)
+#     await sleep(2)
 #     while True:
 #         output = con.send_command('platform configuration channel show status')
 #         if 'succeeded' in output:
@@ -124,9 +125,9 @@ countdown(10)
 #             break
 #         else:
 #             print(f'{c_CYAN}Validating..', end='\r')
-#     sleep(2)
+#     await sleep(2)
 #     print(f'{c_GREEN}Restoring configuration..')
-#     sleep(2)
+#     await sleep(2)
 #     con.send_command(restore_template,
 #                      expect_string='WARNING: This will replace the working configuration. System will reboot.')
 #     output = con.send_command('yes\n',
@@ -135,12 +136,11 @@ countdown(10)
 #         print(f'{c_GREEN}Done..')
 #     else:
 #         print(f'{c_RED}Something went wrong')
-#     sleep(1)
+#     await sleep(1)
 #     countdown(250)
 # 
 # 
 # con = connect_to_radio(ip='192.168.1.1', user='admin', password='admin')
 # print("\n")
 # print(f'{c_GREEN}Gathering info..')
-# countdown(10)
 # # software_dl()
