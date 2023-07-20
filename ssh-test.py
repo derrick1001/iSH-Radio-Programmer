9ae0bedaa387b3b122f21687635271b943362dbe
Derrick Ishman <derrick.ishman@gmail.com>
2023-07-19 21:30:18 -0500

    First python revision of radio programmer

--- ssh-test.py
+++ ssh-test.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3.8
 
 from netmiko import ConnectHandler
-from subprocess import run
 from time import sleep
+from rich.progress import track
+import colorama
 
-ip = input('IP-address: ')
-user = input('User: ')
-password = input('Password: ')
+# Auto reset color back to white after color call
+colorama.init(autoreset=True)
+
+# Colors we use
+c_BLUE = f"{colorama.Style.BRIGHT}{colorama.Fore.BLUE}"
+c_GREEN = f"{colorama.Style.BRIGHT}{colorama.Fore.GREEN}"
+c_MAGENTA = f"{colorama.Style.BRIGHT}{colorama.Fore.MAGENTA}"
+c_CYAN = f"{colorama.Style.BRIGHT}{colorama.Fore.CYAN}"
+c_RED = f"{colorama.Style.BRIGHT}{colorama.Fore.RED}"
+
+ip = input(f'{c_BLUE}IP-address: ')
+user = input(f'{c_BLUE}User: ')
+password = input(f'{c_BLUE}Password: ')
+radio = input(f'{c_BLUE}HI or LO: ').upper()
 
 
 def connect_to_radio(ip, user, password):
@@ -20,19 +32,33 @@ def connect_to_radio(ip, user, password):
     return conn
 
 
-con = connect_to_radio(ip='192.168.1.1', user='admin', password='admin')
+def countdown(second):
+    for n in track(range(second), description=f'{c_GREEN}Rebooting...'):
+        sleep(1)
 
 
 def refresh(cmd):
     while True:
         output = con.send_command(cmd)
-        if '100' in output:
-            print(output)
-            sleep(2)
+        print(output)
+        if 'all components exist' in output:
+            print(f'{c_CYAN}Software is up to date.', end='\r')
             break
-        else:
-            print(output)
+        elif 'Nothing to Update' in output:
+            print(f'{c_CYAN}Already up to date', end='\r')
+            break
+        elif '100' in output:
+            break
+        elif 'download in progress' in output:
+            parse_string = output.split()
+            print(f"{c_CYAN}{parse_string[8].capitalize()} {c_CYAN}{parse_string[9].capitalize()} {c_MAGENTA}{parse_string[10].capitalize()}", end='\r')
             sleep(10)
+        elif 'install started' or 'installation in progress' in output:
+            parse_string = output.split()
+            print(f"{c_CYAN}{parse_string[8].capitalize()} {c_CYAN}{parse_string[9].capitalize()} {c_MAGENTA}{parse_string[10].capitalize()}", end='\r')
+            sleep(10)
+        else:
+            continue
 
 
 def software_dl():
@@ -47,10 +73,13 @@ def software_dl():
                      expect_string='root>',
                      delay_factor=2
                      )
-    refresh(status)
-    print('Software successfully uploaded.')
+    print(f'{c_GREEN}Downloading software..')
     sleep(2)
-    print('Upgrading firmware now..')
+    refresh(status)
+    sleep(3)
+    print(f'{c_GREEN}Software successfully uploaded.')
+    sleep(2)
+    print(f'{c_GREEN}Upgrading firmware..')
     install_func()
 
 
@@ -64,33 +93,53 @@ def install_func():
                      delay_factor=2
                      )
     refresh(status)
-    print('Installation complete.')
+    sleep(3)
+    print(f'{c_GREEN}Installation complete.')
     sleep(2)
-    print('Rebooting...')
-    sleep(180)
+    # countdown(250)
+    # import_func(radio)
 
 
 def import_func(radio_type):
     set_template_params = f'platform configuration channel server set ip-address 192.168.1.30 directory sd1/820S-Builds/ filename 820S-12-0-{radio_type}-template.zip username RSI-Admin password can0py_BAM'
     import_template = 'platform configuration configuration-file import restore-point-1'
     restore_template = 'platform configuration configuration-file restore restore-point-1'
+    con = connect_to_radio(ip='192.168.1.1', user='admin', password='admin')
 
     con.send_command(set_template_params)
     con.send_command(import_template,
                      expect_string='WARNING: This will replace the existing configuration file.',)
     con.send_command('yes\n',
                      expect_string='root>',)
+    print(f'{c_GREEN}Importing template..')
+    sleep(2)
     while True:
         output = con.send_command('platform configuration channel show status')
         if 'succeeded' in output:
-            print('File transfer complete..')
+            print(f'{c_GREEN}File transfer complete..')
             break
         elif 'failure' in output:
-            print('Something went wrong..')
+            print(f'{c_RED}Something went wrong..')
             break
         else:
-            print(output)
+            print(f'{c_CYAN}Validating..', end='\r')
+    sleep(2)
+    print(f'{c_GREEN}Restoring configuration..')
+    sleep(2)
+    con.send_command(restore_template,
+                     expect_string='WARNING: This will replace the working configuration. System will reboot.')
+    output = con.send_command('yes\n',
+                     expect_string='The system is going down for reboot NOW!')
+    if 'NOW!' in output:
+        print(f'{c_GREEN}Done..')
+    else:
+        print(f'{c_RED}Something went wrong')
+    sleep(1)
+    countdown(250)
 
 
-software_dl()
-# import_func(input('HI or LO: ').upper())
+con = connect_to_radio(ip='192.168.1.1', user='admin', password='admin')
+print("\n")
+print(f'{c_GREEN}Gathering info..')
+# software_dl()
+countdown(20)
